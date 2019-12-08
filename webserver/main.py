1ad80b1fa7582b8c038c038c4b4700e37dc5258d
congnt158051 <20158051@sie.edu.vn>
2019-12-08 19:56:44 +0700

    Add post image request

--- webserver/main.py
+++ webserver/main.py
@@ -1,25 +1,37 @@
 import time
+import uuid
+import numpy as np
+from PIL import Image
+from starlette.requests import Request
+import io
+import base64
+import json
+import os
 
 import redis
 from fastapi import FastAPI, File, HTTPException
 
 app = FastAPI()
-cache = redis.StrictRedis(host='redis')
-
-
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
-
+db = redis.StrictRedis(host='redis')
 
 @app.get("/")
 def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
+    return "Hello World!"
+
+@app.post("/predict")
+def predict(request: Request, img_file: bytes=File(...)):
+    data = {"success": False}
+    if request.method == "POST":
+        image = Image.open(io.BytesIO(img_file))
+        if image.mode != "RGB":
+            image = image.convert("RGB")
+        image = np.array(image)
+        height_image = image.shape[0]
+        width_image = image.shape[1]
+        
+        image = image.copy(order="C")
+
+        k = str(uuid.uuid4())
+        image = base64.b64encode(image).decode("utf-8")
+        d = {"id": k, "image": image, "height":height_image, "width": width_image}
+        db.rpush('image_queue', json.dumps(d))
