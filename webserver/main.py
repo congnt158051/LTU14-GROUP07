b708c789c67c8779a93de6afad2a5acfea590913
duonggnt <ntduong997@gmail.com>
2019-12-09 15:02:57 +0700

    add response after post image

--- webserver/main.py
+++ webserver/main.py
@@ -14,6 +14,8 @@ from fastapi import FastAPI, File, HTTPException
 app = FastAPI()
 db = redis.StrictRedis(host=os.environ.get("REDIS_HOST"))
 
+CLIENT_MAX_TRIES = int(os.environ.get("CLIENT_MAX_TRIES"))
+
 @app.get("/")
 def hello():
     return "Hello World!"
@@ -35,3 +37,23 @@ def predict(request: Request, img_file: bytes=File(...)):
         image = base64.b64encode(image).decode("utf-8")
         d = {"id": k, "image": image, "height":height_image, "width": width_image}
         db.rpush(os.environ.get("IMAGE_QUEUE"), json.dumps(d))
+        num_tries = 0
+        while num_tries < CLIENT_MAX_TRIES:
+            num_tries += 1
+
+            output = db.get(k)
+
+            if output is not None:
+                output = output.decode("utf-8")
+                data["detection"] = json.loads(output)
+
+                db.delete(k)
+                break
+            time.sleep(float(os.environ.get("CLIENT_SLEEP")))
+
+            data["success"] = True
+        else:
+            raise HTTPException(status_code=400, detail="Request failed after {} tries".format(CLIENT_MAX_TRIES))
+
+    # Return the data dictionary as a JSON response
+    return data
